c37e2b4d5a2a57518f4916c86705ae3c7218a66c
harshpreet931 <hsanime@gmail.com>
2023-06-12 17:59:16 +0530

    part1 arraySearchInsertDelete

---
+++ DAY 1/arraySearchInsertDelete.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class arraySearchInsertDelete {
+    static int findElement(int arr[], int n, int key){
+        for(int i=0;i<n;i++)
+            if(arr[i]==key) return i;
+        return -1;
+    }
+    static void insertElement(int arr[], int n,int x,int pos){
+        for(int i=n-1;i>=pos;i--)
+            arr[i+1] = arr[i];
+            arr[pos] = x;
+    }
+    static int deleteElement(int arr[], int n, int key){
+        int pos = findElement(arr, n, key);
+
+        if(pos==-1){
+            System.out.println("Element not found");
+            return n;
+        }
+
+        int i;
+        for(i = pos;i<n-1;i++)
+            arr[i] = arr[i+1];
+        
+        return n-1;
+
+    }
+}
