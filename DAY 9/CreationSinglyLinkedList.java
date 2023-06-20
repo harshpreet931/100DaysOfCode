7064513e9cfa20c59edc3bd4e60de4c0c2b2a33c
harshpreet931 <hsanime@gmail.com>
2023-06-20 21:12:46 +0530

    Day 9

---
+++ DAY 9/CreationSinglyLinkedList.java
@@ -0,0 +1,10 @@
+public class CreationSinglyLinkedList {
+    class Node{
+        int data;
+        Node next;
+        Node(int d){
+            data = d;
+            next = null;
+        }
+    }
+}
